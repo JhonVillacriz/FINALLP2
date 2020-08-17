20498c4013e59a25f25b3c12084f62befed078c4
Jhon Villacriz <jairosky.22.vm@gmail.com>
2020-08-17 00:37:02 -0500

    Arquitectura MVC + estudiante carrera .java listo!!

---
+++ src/java/Modelo/Estudiante.java
@@ -0,0 +1,75 @@
+package Modelo;
+
+public class Estudiante {
+    private int idestudiante;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String codigo;
+    private String direccion;
+    private String estado;
+
+    public Estudiante() {
+    }
+
+    public int getIdestudiante() {
+        return idestudiante;
+    }
+
+    public void setIdestudiante(int idestudiante) {
+        this.idestudiante = idestudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+   
+    
+    
+    
+}
